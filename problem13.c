fc135fd526479cafd502bd2a598526a6f81e7b75
Meikumar <meikumar49@gmail.com>
2026-01-26 00:23:58 +0530

    Add files via upload

---
+++ problem13.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    int b = 5;
+    int c = 20;
+    int d = 10;
+
+    int e = a+b;
+    int f = c+d;
+
+    printf("%d \n", e);
+    printf("%d", f);
+
+    return 0;
+}
