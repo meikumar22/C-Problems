21f367245549b5fbb1f1d3897cf58abe916cfb4f
Meikumar <meikumar49@gmail.com>
2026-02-03 14:50:48 +0530

    Add files via upload

---
+++ 1Darray1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(){
+    int size;
+    scanf("%d", &size);
+    int arr[size];
+    
+    for(int i=0; i<size; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    int max=-1;
+    int max1=-1;
+    
+    for(int j=0; j<size; j++){
+        if(arr[j]>max){
+            max1 = max;
+            max = arr[j];
+        }
+        else if(arr[j]>max1 && arr[j]!=max){
+            max1 = arr[j];
+        }
+    }
+    printf("second highest : %d", max1);
+
+    return 0;
+}
