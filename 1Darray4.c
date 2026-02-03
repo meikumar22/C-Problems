f10ae3ccac0c1039a6c85489d643e114e37023bd
Meikumar <meikumar49@gmail.com>
2026-02-03 14:51:59 +0530

    Add files via upload

---
+++ 1Darray4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int size;
+    scanf("%d", &size);
+    int arr[size];
+
+    for(int i=0; i<size; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    for(int j=size-1; j>=0; j--){
+        printf("%d ", arr[j]);
+    }
+    return 0;
+}
