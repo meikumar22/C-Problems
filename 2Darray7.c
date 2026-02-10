58b3cc59f7003ae34cee57cab5bebf27fdfd8b53
Meikumar <meikumar49@gmail.com>
2026-02-09 20:43:09 -0800

    Update 2Darray7.c

--- 2Darray7.c
+++ 2Darray7.c
@@ -1,26 +1,37 @@
-#include <stdio.h>
+#include<stdio.h>
 int main(){
-    int r,c;
+    int r,c,count=0;
     printf("Enter the r and c value : ");
     scanf("%d %d", &r, &c);
-    int arr[r][c];
+    int a[r][c];
 
-    for(int i=0; i<r; i++){
-        for(int j=0; j<c; j++){
-            scanf("%d", &arr[i][j]);
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&a[i][j]);
         }
     }
 
-    int a = 0;
-    int b = 0;
-
-    for(int i=0; i<r; i++){
-        for(int j=0; j<c; j++){
-            if(arr[i][j]<a){
-                a = arr[i][j];
-                b++;
+    
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            int small=1;
+            int large=1;
+            for(int k=0;k<c;k++){
+                if(a[i][k]<a[i][j]){
+                    small=0;
+                    break;
+                }
+            }
+            for(int l=0;l<r;l++){
+                if(a[l][j]>a[i][j]){
+                    large=0;
+                    break;
+                }
+            }
+            if(small&&large){
+                count++;
             }
         }
     }
-    printf("%d", b);
+    printf("%d",count);
 }
