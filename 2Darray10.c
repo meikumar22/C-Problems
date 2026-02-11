f32d0c20261da25e1bf3d347568edfab089754a3
Meikumar <meikumar49@gmail.com>
2026-02-11 14:17:52 +0530

    Add files via upload

---
+++ 2Darray10.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(){
+    int r,c;
+    printf("Enter the r and c value : ");
+    scanf("%d %d", &r, &c);
+    int arr[r][c];
+
+    for(int i=0; i<r; i++){
+        for(int j=0; j<c; j++){
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    int index = 0; 
+    int b = 0;
+
+    for(int i=0; i<r; i++){
+        int a = 0;
+        for(int j=0; j<c; j++){
+            a += arr[i][j];
+        }
+        if(i==0 || a>b){
+            b = a;
+            index = i;
+        }
+    }
+    printf("%d ", index);
+}
