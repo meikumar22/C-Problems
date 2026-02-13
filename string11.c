e3366a2a7d6b8e410cabb958b1090d02a5aeed5c
Meikumar <meikumar49@gmail.com>
2026-02-13 15:10:39 +0530

    Add files via upload

---
+++ string11.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char str[100];
+    fgets(str,sizeof(str), stdin);
+    int len=strlen(str);
+
+    int count = 0;
+
+    for(int i=0; i<len; i++){
+        if(str[i]>='0' && str[i]<='9'){
+            count++;
+        }
+    }
+    printf("%d", count);
+}
