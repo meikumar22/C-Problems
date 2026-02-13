0e6c65f193b3b7cc9807cc5b20056ffa9e7d40e8
Meikumar <meikumar49@gmail.com>
2026-02-13 15:09:07 +0530

    Add files via upload

---
+++ string5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char str[100];
+    scanf("%s", &str);
+    int len=strlen(str);
+
+    int count = 1;
+
+    for(int i=0; i<len/2; i++){
+        if(str[i]!=str[len-1-i]){
+            count = 0;
+            break;
+        }
+    }
+    if(count==1){
+        printf("Palindrome");
+    }
+    else{
+        printf("It is not palindrome");
+    }
+    return 0;
+}
