c24de2d18c523f22ef08ebdc35e9c3c0dd3d6eb6
Meikumar <meikumar49@gmail.com>
2026-02-13 15:09:24 +0530

    Add files via upload

---
+++ string6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char str[100];
+    //scanf("%s", &str);
+    fgets(str,sizeof(str), stdin);
+    int len=strlen(str);
+
+    int count = 0;
+
+    for(int i=0; i<len; i++){
+        if(str[i]=='a' || str[i]=='A' || str[i]=='e' || str[i]=='E' || str[i]=='i' || str[i]=='I' ||
+        str[i]=='o' || str[i]=='O' || str[i]=='u' || str[i]=='U'){
+            count++;
+        }
+    }
+    printf("%d", count);
+}
