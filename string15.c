7581569d0a4d7852279f457ca29d63e8e0f619ed
Meikumar <meikumar49@gmail.com>
2026-02-13 15:11:06 +0530

    Add files via upload

---
+++ string15.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str[100];
+    fgets(str,sizeof(str), stdin);
+    int len=strlen(str);
+    int word = 0;
+    int count = 0;
+
+    for(int i=0; i<len; i++){
+        if(str[i]==' '){
+            word = str[i];
+            count++; 
+        }
+    }
+
+    printf("%d ", count+1);
+}
